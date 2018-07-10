97a9393504dc582d93806806d46c35cf412388b3
ajilantang <ajilantang@gmail.com>
2018-07-10 22:53:45 +0700

    day 3

---
+++ DailyTask/src/checkpassword.js
@@ -0,0 +1,42 @@
+// @flow
+function checkPassword(password) {
+  let character = password.split('');
+  if (password.length < 6) {
+    return {
+      success: false,
+      alert: 'Poor password, required both lower and upper case character',
+    };
+  }
+  if (
+    isRequiredPass(character, 'a', 'z') &&
+    isRequiredPass(character, 'A', 'Z')
+  ) {
+    return {success: true, alert: 'Good password'};
+  }
+  return {
+    success: false,
+    alert: 'Poor password, required both lower and upper case character ',
+  };
+}
+
+function isRequiredPass(listChar, minBound, maxBound) {
+  let minChar = minBound.charCodeAt(0);
+  let maxChar = maxBound.charCodeAt(0);
+  for (let char of listChar) {
+    if (char.charCodeAt(0) >= minChar && char.charCodeAt(0) <= maxChar) {
+      return true;
+    }
+  }
+  return false;
+}
+
+it('should return 4', () => {
+  expect(checkPassword('asdasdasd')).toEqual({
+    success: false,
+    alert: 'Poor password, required both lower and upper case character ',
+  });
+  expect(checkPassword('AsjsnaS')).toEqual({
+    success: true,
+    alert: 'Good password',
+  });
+});
---
+++ DailyTask/src/counter.js
@@ -0,0 +1,27 @@
+// @flow
+
+function createCounter() {
+  let count = 0;
+  return {
+    inc: () => {
+      count += 1;
+    },
+    dec: () => {
+      count -= 1;
+    },
+    getCount: () => count,
+  };
+}
+
+it('should return 4', () => {
+  let counter = createCounter();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.dec();
+  counter.dec();
+  expect(counter.getCount()).toEqual(4);
+});
