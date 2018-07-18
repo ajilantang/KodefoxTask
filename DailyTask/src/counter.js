12ec313f6954e632a538b8c7b2af30f526fdd5a8
david-dss <david@dihardjasoftware.com>
2018-07-18 14:39:06 +0700

    uncomment counter testing

--- DailyTask/src/counter.js
+++ DailyTask/src/counter.js
@@ -14,15 +14,15 @@ export default function createCounter() {
   };
 }
 
-// it('should return 4', () => {
-//   let counter = createCounter();
-//   counter.inc();
-//   counter.inc();
-//   counter.inc();
-//   counter.inc();
-//   counter.inc();
-//   counter.inc();
-//   counter.dec();
-//   counter.dec();
-//   expect(counter.getCount()).toEqual(4);
-// });
+it('should return 4', () => {
+  let counter = createCounter();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.inc();
+  counter.dec();
+  counter.dec();
+  expect(counter.getCount()).toEqual(4);
+});
